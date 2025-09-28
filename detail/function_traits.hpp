721da16c3a517c74a2d6fabc539a036d6b51f5d3
treywalg <grygoriyakimenko@mail.ru>
2025-09-28 19:07:30 +0300

    Added function_traits for future features

---
+++ detail/function_traits.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstddef>
+#include <tuple>
+
+namespace router {
+namespace detail {
+
+template <typename T>
+struct function_traits
+  : function_traits<decltype(&T::operator())>
+{};
+
+template <typename class_type, typename return_type, typename... Args>
+struct function_traits<return_type(class_type::*)(Args...) const> {
+  static constexpr size_t arity = sizeof...(Args);
+
+  using result_type = return_type;
+  using args_tuple = std::tuple<Args...>;
+};
+
+template <typename class_type, typename return_type, typename... Args>
+struct function_traits<return_type(class_type::*)(Args...)> {
+  static constexpr size_t arity = sizeof...(Args);
+
+  using result_type = return_type;
+  using args_tuple = std::tuple<Args...>;
+};
+
+template <typename return_type, typename... Args>
+struct function_traits<return_type(*)(Args...)> {
+  static constexpr size_t arity = sizeof...(Args);
+
+  using result_type = return_type;
+  using args_tuple = std::tuple<Args...>;
+};
+
+template <typename T>
+using handler_args_t = typename function_traits<T>::args_tuple;
+
+} // namespace detail
+} // namespace router
