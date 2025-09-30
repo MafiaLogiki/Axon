0f3f3aeee4f90c3253506957ee330e4b09a372c7
treywalg <grygoriyakimenko@mail.ru>
2025-09-30 21:03:31 +0300

    Started listener class

---
+++ sn_router/listener/core.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <sn_router/detail/http_connection.hpp>
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+
+namespace listener {
+namespace detail {
+
+template <typename session>
+class base_listener {
+  
+  base_listener(boost::asio::io_context& io,
+         boost::asio::ip::address address,
+         unsigned short port)
+      : io(io), 
+        acceptor(io, {address, port}) 
+  {}
+
+public:
+
+  base_listener(base_listener&&) = default;
+
+  void serveHTTP() {
+    start_http_server();
+    io.run();
+  }
+
+private:
+
+  void start_http_server() {
+    acceptor.async_accept(
+      [this](boost::beast::error_code ec, boost::asio::ip::tcp::socket socket) {
+          if(!ec)
+            std::make_shared<session>(std::move(socket), *this)->start();
+          start_http_server();
+      }
+    ); 
+  }
+
+  boost::asio::io_context& io;
+  boost::asio::ip::tcp::acceptor acceptor;
+};
+
+} // namespace detail
+} // namespace listener
+
+namespace listener {
+  using core = listener::detail::base_listener<router::detail::http_connection<int>>;
+}
--- sn_router/mux.hpp
+++ sn_router/mux.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
 #include <sn_router/router/core.hpp>
+#include <sn_router/listener/core.hpp>
 
 template <typename router_engine, typename listener_engine>
 class base_mux {
-
+  using handler_type = decltype(router_engine::get_handler);
 };
 
-using mux = base_mux<router::core, int>;
+using mux = base_mux<router::core, listener::core>;
