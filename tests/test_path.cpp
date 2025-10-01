ce0944f298fd2ac0ac4f119536f34f40f2320757
treywalg <grygoriyakimenko@mail.ru>
2025-10-01 16:52:27 +0300

    Added test for path with multiple variables

--- tests/test_path.cpp
+++ tests/test_path.cpp
@@ -41,3 +41,47 @@ TEST(matching_test, simple_int_path_test) {
 
   ASSERT_EQ(d.front(), 3);
 }
+
+
+TEST(matching_test, multy_variable_path_test) {
+
+  auto r = router::core::create_router();
+  
+  std::deque<int> d;
+
+  r->GET<"/test/{int:id}/path/{int:id2}">([&d](router::extract::path<int, int> p){
+    d.push_back(std::get<0>(p));
+    d.push_back(std::get<1>(p));
+  });
+
+  r->GET<"/test/{int:id}/path2/{int:id2}">([&d](router::extract::path<int, int> p){
+    d.push_back(std::get<0>(p));
+    d.push_back(std::get<1>(p));
+  });
+
+  sn::request_type  req; 
+  sn::response_type res; 
+
+  sn::request_type  req2; 
+  sn::response_type res2;
+
+  req.target("/test/1/path/2");
+  req2.target("/test/3/path2/4");
+
+  auto h1 = r->get_handler(req.target(), http::verb::get);
+  auto h2 = r->get_handler(req2.target(), http::verb::get);
+
+  h1(std::move(req), res);
+  h2(std::move(req2), res2);
+
+  ASSERT_EQ(d.front(), 1);
+  d.pop_front();
+
+  ASSERT_EQ(d.front(), 2);
+  d.pop_front();
+
+  ASSERT_EQ(d.front(), 3);
+  d.pop_front();
+
+  ASSERT_EQ(d.front(), 4);
+}
