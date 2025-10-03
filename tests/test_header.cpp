cf82e7b1d2f03569da8013dd4d617c6bd6b69fbb
treywalg <grygoriyakimenko@mail.ru>
2025-10-03 14:46:51 +0300

    Added test for empty header

--- tests/test_header.cpp
+++ tests/test_header.cpp
@@ -12,7 +12,7 @@
 
 namespace http = boost::beast::http;
 
-TEST(Header, Simple)
+TEST(Header, simple_header)
 {
 
   auto r = router::core::create_router();
@@ -35,4 +35,30 @@ TEST(Header, Simple)
   h1(std::move(req), res);
 
   ASSERT_EQ(token, "some-token");
+}
+
+
+TEST(Header, empty_header)
+{
+
+  auto r = router::core::create_router();
+
+  sn::request_type req;
+  sn::response_type res;
+
+  req.method(http::verb::get);
+  req.target("/");
+  req.set("Authorization", "some-token");
+  
+  std::string token;
+
+  r->GET<"/">([&token](router::extract::header<"empty-field"> h){
+    token = h.get_value();
+  });
+
+  auto h1 = r->get_handler(req.target(), http::verb::get);
+
+  h1(std::move(req), res);
+
+  ASSERT_EQ(token, "");
 }
